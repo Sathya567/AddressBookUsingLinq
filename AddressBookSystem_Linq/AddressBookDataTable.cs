91f15d64ec44e578a93e3ba2622d8178d92004f0
BootCampBL <satyabhandari43@gmail.com>
2021-08-19 23:02:41 +0530

    [satya] added- Editing records by first name

--- AddressBookSystem_Linq/AddressBookDataTable.cs
+++ AddressBookSystem_Linq/AddressBookDataTable.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Linq;
 using System.Text;
 
 namespace AddressBookSystem_Linq
@@ -34,6 +35,26 @@ namespace AddressBookSystem_Linq
             dataTable.Rows.Add("naidu", "Gani", "Marathalli", "Banglore", "Karnataka", "500056", "9632544570", "vishal@gmail.com");
             dataTable.Rows.Add("Ganesh", "Reddy", "vesarpadi", "Chennai", "TamilNadu", "600341", "97456321011", "reddy@gmail.com");
             return dataTable;
+        }
+        public void AddContact(AddressBookModel model)
+        {
+            dataTable.Rows.Add(model.First_Name, model.Last_Name, model.Address, model.City,
+                model.State, model.Zip, model.Phone_Number, model.Email);
+            Console.WriteLine("Contact Added Succesfully...");
+        }
+        public void EditContact(AddressBookModel model)
+        {
+            var recordData = dataTable.AsEnumerable().Where(data => data.Field<string>("First_Name") == model.First_Name).First();
+            if (recordData != null)
+            {
+                recordData.SetField("Last_Name", model.Last_Name);
+                recordData.SetField("Address", model.Address);
+                recordData.SetField("City", model.City);
+                recordData.SetField("State", model.State);
+                recordData.SetField("Zip", model.Zip);
+                recordData.SetField("Phone_Number", model.Phone_Number);
+                recordData.SetField("Email", model.Email);
+            }
         }
         public void Display()
         {
