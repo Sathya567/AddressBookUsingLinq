dc2775d328329fa36cd8eb5c87cc1e1dbd859a08
BootCampBL <satyabhandari43@gmail.com>
2021-08-19 22:47:41 +0530

    [satya] added- Create AddressBook database table

---
+++ AddressBookSystem_Linq/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AddressBookSystem_Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome_To_AddressBookSystem_Linq");
+            AddressBookModel model = new AddressBookModel();
+            AddressBookDataTable dataTable = new AddressBookDataTable();
+            dataTable.CreateTable(model);
+        }
+    }
+}
